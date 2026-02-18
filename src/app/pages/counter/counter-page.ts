24a9268956a90c63958ae12d81e1ab09848bf1e1
Miguel <miguef.c11@gmail.com>
2026-02-18 16:33:37 -0300

    cambios generales para iniciar el proyecto, en counter se agrego el contenido principal

---
+++ src/app/pages/counter/counter-page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    template:`
+    <h1> Hola gato</h1>
+    <h2>CounterComponent Page</h2>
+    `
+})
+
+
+export class CounterPage {
+
+}
