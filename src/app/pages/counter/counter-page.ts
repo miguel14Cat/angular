8ec4dd709f06b8c909b0f5be0b1ab0a16996476f
Miguel <miguef.c11@gmail.com>
2026-02-20 15:58:22 -0300

    se cambio todas las funcionalidades visuales a html

--- src/app/pages/counter/counter-page.ts
+++ src/app/pages/counter/counter-page.ts
@@ -1,18 +1,26 @@
 import { Component } from '@angular/core';
 
 @Component({
-    template:`
-    <h1>Counter: {{ counter}} </h1>
-   <button (click) ="incremento(1)">+1</button>
-    `
+   templateUrl: './counter-page.html',
+   styles:`
+    button {
+        padding: 5px;
+        margin: 5px 10px;
+        width: 75px;
+    }
+   `
 })
 
 
 export class CounterPage {
 
-    counter = 20;
+    counter = 10;
     incremento(value: number) {
         this.counter += value;
+    }
+
+    resetContador(){
+        this.counter = 10;
     }
 
 }
