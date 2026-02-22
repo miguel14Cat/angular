93fa267e9e778f499429bd022dd9be7aaa7549d6
Miguel <miguef.c11@gmail.com>
2026-02-22 12:42:09 -0300

    se agrego mini tarea de aprendizaje

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { CounterPage } from './pages/counter/counter-page';
+import { HeroPageComponent } from './pages/hero/hero-page.component';
 
 export const routes: Routes = [
     {
         path:'',
         component: CounterPage,
+    },
+    {
+    path: 'hero',
+    component: HeroPageComponent,
     }
 ];
---
+++ src/app/pages/hero/hero-page.component.ts
@@ -0,0 +1,24 @@
+import { Component, signal } from "@angular/core";
+
+@Component({
+    templateUrl: './hero-page.component.html'
+})
+
+export class HeroPageComponent {
+name = signal('Ironman') 
+age = signal(45)
+getHeroDescription() {
+    return `${this.name()} - ${this.age()}`;
+}
+changeHero() {
+    this.name.set('spiderman');
+    this.age.set(22);
+}
+changeAge() {
+this.age.set(60);
+}
+resetForm() {
+    this.name.set('Ironman')
+    this.age.set(45)
+    }
+}
