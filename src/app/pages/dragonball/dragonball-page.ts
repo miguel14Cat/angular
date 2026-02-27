b58a3bf641562ec598129d67a05d7b6e300255c9
Miguel <miguef.c11@gmail.com>
2026-02-27 17:27:28 -0300

    actualizaciones menores a la tarea dragonball

--- src/app/pages/dragonball/dragonball-page.ts
+++ src/app/pages/dragonball/dragonball-page.ts
@@ -12,11 +12,28 @@ interface character {
     //imports: [NgClass],
 })
 export class DragonballPage{
+
+    name = signal('Gohan');
+    power = signal(100);
+
     characters = signal<character[]>([
         {id: 1, name:'Goku', power: 9001},
         {id: 2, name:'Vegeta', power: 8000},
         {id: 3, name:'Piccolo', power: 3000},
+        {id: 4, name:'Yamcha', power: 500},
     ]);
+
+    addCharacter() {
+        if (this.name() || this.power() || this.power() <= 0){
+            return;
+        }
+        const newCharacter : character = {
+            id: this.characters().length + 1,
+            name: this.name(),
+            power: this.power(),
+        };
+        this.characters().push(newCharacter);
+    }
 
    // powerClasses = computed(() => {
    //     return{
